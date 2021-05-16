90ecb8ff2f669e854574ff22c7bdb59dd55c91c3
HunterPlus <zlw08140957@gmail.com>
2021-05-16 17:16:13 +0800

    Create bzero.s

---
+++ bzero.s
@@ -0,0 +1,15 @@
+;void bzero(void* addr, long unsigned len);
+
+section .text
+    global my_bzero
+
+my_bzero:
+.loop:
+    dec rsi
+    cmp rsi, 0
+    jl .ret
+    mov byte [rdi + rsi], 0
+    jmp .loop
+
+.ret:
+    ret
